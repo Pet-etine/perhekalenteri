9f7e95172d11008ff0e112e3c1b18535a9618440
Petri Venäläinen-Kuusela <p97755@edu.sasky.fi>
2025-09-30 18:47:58 +0300

    Titan One lisätty otsikkofonttilistalle

--- deploy.php
+++ deploy.php
@@ -1,24 +1,8 @@
 <?php
 namespace Deployer;
-
 require 'recipe/common.php';
 
-// Project name
-set('application', 'perhekalenteri');
-
-// Project repository
 set('repository', 'git@github.com:Pet-etine/perhekalenteri.git');
-
-// [Optional] Allocate tty for git clone. Default value is false.
-// set('git_tty', true);
-
-// Shared files/dirs between deploys 
-set('shared_files', []);
-set('shared_dirs', []);
-
-// Writable dirs by web server 
-set('writable_dirs', []);
-
 
 // Hosts
 host('neutroni.hayo.fi')
@@ -26,25 +10,15 @@ host('neutroni.hayo.fi')
     ->set('deploy_path', '~/public_html/{{application}}');
 
 
-    
-
-// Tasks
-
-desc('Deploy your project');
+desc('Deploy');
 task('deploy', [
-    'deploy:info',
-    'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:clear_paths',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',
-    'success'
+  'deploy:prepare',
+  'deploy:release',
+  'deploy:update_code',
+  'deploy:shared',
+  'deploy:vendors',
+  'deploy:writable',
+  'deploy:symlink',
+  'deploy:unlock',
+  'cleanup',
 ]);
-
-// [Optional] If deploy fails automatically unlock.
-after('deploy:failed', 'deploy:unlock');
